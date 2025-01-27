f31fa5b8e281fb1c9f3d0bbb9ef6620b34e660b6
Bruce Chibisa <pvlm5xi2@students.codeinstitute.net>
2025-01-27 17:41:21 +0000

    docs: Add question variables to script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const question = [
+    {
+        question: 'Will this code work after you push it?'
+        answers: [
+        { text: "Yes", correct: true}
+        { text: "Maybe", correct: false}
+        { text: "Highly doubt it", correct: false}
+        { text: "Ask the computer nicely", correct: false}
+        ]
+    },
+    {
+        question: 'Did you fix any and all bugs before pushing this code?'
+        answers: [
+        { text: "Yes", correct: false}
+        { text: "Maybe", correct: false}
+        { text: "Highly doubt it", correct: true}
+        { text: "Ask the computer nicely", correct: false}
+        ]
+    },
+    {
+        question: 'what colour is the sky?'
+        answers: [
+        { text: "Black", correct: true}
+        { text: "Blue", correct: false}
+        { text: "Depends on the time-zone", correct: false}
+        { text: "Baige", correct: false}
+        ]
+    },
+]

b0ff59c01ad69c0031c620741335ce9dd2462862
Bruce Chibisa <pvlm5xi2@students.codeinstitute.net>
2025-01-28 20:18:09 +0000

    doc: Add variables for questions and answers to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,53 @@
-const question = [
-    {
-        question: 'Will this code work after you push it?'
-        answers: [
-        { text: "Yes", correct: true}
-        { text: "Maybe", correct: false}
-        { text: "Highly doubt it", correct: false}
-        { text: "Ask the computer nicely", correct: false}
-        ]
+const question = [{
+        question: 'Will this code work after you push it?',
+        answers: [{
+            text: "Yes",
+            correct: true
+        }, {
+            text: "Maybe",
+            correct: false
+        }, {
+            text: "Highly doubt it",
+            correct: false
+        }, {
+            text: "Ask the computer nicely",
+            correct: false
+        },]
     },
     {
-        question: 'Did you fix any and all bugs before pushing this code?'
-        answers: [
-        { text: "Yes", correct: false}
-        { text: "Maybe", correct: false}
-        { text: "Highly doubt it", correct: true}
-        { text: "Ask the computer nicely", correct: false}
-        ]
+        question: 'Did you fix any and all bugs before pushing this code?',
+        answers: [{
+            text: "Yes",
+            correct: false
+        }, {
+            text: "Maybe",
+            correct: false
+        }, {
+            text: "Highly doubt it",
+            correct: true
+        }, {
+            text: "Ask the computer nicely",
+            correct: false
+        }, ]
     },
     {
-        question: 'what colour is the sky?'
-        answers: [
-        { text: "Black", correct: true}
-        { text: "Blue", correct: false}
-        { text: "Depends on the time-zone", correct: false}
-        { text: "Baige", correct: false}
-        ]
-    },
-]
+        question: 'what colour is the sky?',
+        answers: [{
+            text: "Black",
+            correct: true
+        }, {
+            text: "Blue",
+            correct: false
+        }, {
+            text: "Depends on the time-zone",
+            correct: false
+        }, {
+            text: "Baige",
+            correct: false
+        }, ]
+    }
+];
+const questionElement = document.getElementById('question');
+const answersButton = document.getElementById('answers');
+const nextButton = document.getElementById('next-btn');
+
